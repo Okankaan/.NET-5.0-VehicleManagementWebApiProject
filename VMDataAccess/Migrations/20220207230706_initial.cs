bc3fa6ed9b8c32241e9e431f3f07e717e208b482
Okankaan <okankaan.cetinkaya@gmail.com>
2022-02-08 02:09:12 +0300

    Seed data created.

--- VMDataAccess/Migrations/20220207230706_initial.cs
+++ VMDataAccess/Migrations/20220207230706_initial.cs
@@ -160,7 +160,7 @@ namespace VMDataAccess.Migrations
                     Id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    BrandId = table.Column<long>(type: "bigint", nullable: false),
+                    ModelId = table.Column<long>(type: "bigint", nullable: false),
                     ColorId = table.Column<long>(type: "bigint", nullable: false),
                     UserId = table.Column<long>(type: "bigint", nullable: false),
                     Active = table.Column<bool>(type: "bit", nullable: false)
@@ -169,15 +169,15 @@ namespace VMDataAccess.Migrations
                 {
                     table.PrimaryKey("PK_Vehicles", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Vehicles_Brands_BrandId",
-                        column: x => x.BrandId,
-                        principalTable: "Brands",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
                         name: "FK_Vehicles_Colors_ColorId",
                         column: x => x.ColorId,
                         principalTable: "Colors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
@@ -186,6 +186,126 @@ namespace VMDataAccess.Migrations
                         principalTable: "Users",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authorities",
+                columns: new[] { "Id", "Active", "Name" },
+                values: new object[,]
+                {
+                    { 1L, true, "Vehicle Insert Authority" },
+                    { 2L, true, "Vehicle Update Authority" },
+                    { 3L, true, "Vehicle Delete Authority" },
+                    { 4L, true, "Vehicle List Authority" },
+                    { 5L, true, "Brand List Authority" },
+                    { 6L, true, "Model List Authority" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Brands",
+                columns: new[] { "Id", "Active", "Name" },
+                values: new object[,]
+                {
+                    { 1L, true, "Bmw" },
+                    { 2L, true, "Toyota" },
+                    { 3L, true, "Fiat" },
+                    { 4L, true, "Opel" },
+                    { 5L, true, "Audi" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Colors",
+                columns: new[] { "Id", "Active", "Name" },
+                values: new object[,]
+                {
+                    { 5L, true, "Space Grey" },
+                    { 4L, true, "Black" },
+                    { 2L, true, "Red" },
+                    { 1L, true, "White" },
+                    { 3L, true, "Night Blue" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Active", "Name" },
+                values: new object[,]
+                {
+                    { 1L, true, "System Administrator" },
+                    { 2L, true, "Vehicle Administrator" },
+                    { 3L, true, "Brand Administrator" },
+                    { 4L, true, "Model Administrator" },
+                    { 5L, true, "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Active", "EMail", "Name", "Password" },
+                values: new object[,]
+                {
+                    { 4L, true, "sven.ottlieb@test.com", "Sven Ottlieb", "1234" },
+                    { 1L, true, "okankaan.cetinkaya@gmail.com", "Okan Kaan Cetinkaya", "1234" },
+                    { 2L, true, "maria.anders@test.com", "Maria Anders", "1234" },
+                    { 3L, true, "antonio.moreno@test.com", "Antonio Moreno Taquería", "1234" },
+                    { 5L, true, "carine.schmitt@test.com", "Carine Schmitt", "1234" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AuthRoles",
+                columns: new[] { "Id", "Active", "AuthorityId", "RoleId" },
+                values: new object[,]
+                {
+                    { 8L, true, 2L, 2L },
+                    { 15L, true, 6L, 5L },
+                    { 14L, true, 5L, 5L },
+                    { 13L, true, 4L, 5L },
+                    { 12L, true, 6L, 4L },
+                    { 1L, true, 1L, 1L },
+                    { 2L, true, 2L, 1L },
+                    { 3L, true, 3L, 1L },
+                    { 4L, true, 4L, 1L },
+                    { 5L, true, 5L, 1L },
+                    { 6L, true, 6L, 1L },
+                    { 7L, true, 1L, 2L },
+                    { 11L, true, 5L, 3L },
+                    { 9L, true, 3L, 2L },
+                    { 10L, true, 4L, 2L }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "Id", "Active", "BrandId", "Name" },
+                values: new object[,]
+                {
+                    { 3L, true, 1L, "x5" },
+                    { 4L, true, 4L, "Corsa" },
+                    { 5L, true, 2L, "Corolla" },
+                    { 2L, true, 2L, "Rav 4" },
+                    { 1L, true, 2L, "Yaris" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "Id", "Active", "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { 1L, true, 1L, 1L },
+                    { 2L, true, 2L, 2L },
+                    { 3L, true, 3L, 3L },
+                    { 4L, true, 4L, 4L },
+                    { 5L, true, 5L, 5L }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vehicles",
+                columns: new[] { "Id", "Active", "ColorId", "ModelId", "Price", "UserId" },
+                values: new object[,]
+                {
+                    { 3L, true, 4L, 3L, 230000m, 5L },
+                    { 2L, true, 5L, 1L, 150000m, 1L },
+                    { 1L, true, 2L, 2L, 200000m, 1L },
+                    { 5L, true, 2L, 2L, 200000m, 3L },
+                    { 6L, true, 3L, 5L, 300000m, 3L },
+                    { 4L, true, 2L, 4L, 500000m, 4L }
                 });
 
             migrationBuilder.CreateIndex(
@@ -214,14 +334,14 @@ namespace VMDataAccess.Migrations
                 column: "UserId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_Vehicles_BrandId",
-                table: "Vehicles",
-                column: "BrandId");
-
-            migrationBuilder.CreateIndex(
                 name: "IX_Vehicles_ColorId",
                 table: "Vehicles",
                 column: "ColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_ModelId",
+                table: "Vehicles",
+                column: "ModelId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Vehicles_UserId",
@@ -233,9 +353,6 @@ namespace VMDataAccess.Migrations
         {
             migrationBuilder.DropTable(
                 name: "AuthRoles");
-
-            migrationBuilder.DropTable(
-                name: "Models");
 
             migrationBuilder.DropTable(
                 name: "UserRoles");
@@ -250,13 +367,16 @@ namespace VMDataAccess.Migrations
                 name: "Roles");
 
             migrationBuilder.DropTable(
-                name: "Brands");
-
-            migrationBuilder.DropTable(
                 name: "Colors");
 
             migrationBuilder.DropTable(
+                name: "Models");
+
+            migrationBuilder.DropTable(
                 name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
         }
     }
 }
--- VMDataAccess/VMDBContext.cs
+++ VMDataAccess/VMDBContext.cs
@@ -25,5 +25,100 @@ namespace VMDataAccess
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
 
+        /// <summary>
+        /// Test / SEED DATA
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Authority>().HasData(
+                new Authority { Id = 1, Name = "Vehicle Insert Authority", Active = true },
+                new Authority { Id = 2, Name = "Vehicle Update Authority", Active = true },
+                new Authority { Id = 3, Name = "Vehicle Delete Authority", Active = true },
+                new Authority { Id = 4, Name = "Vehicle List Authority", Active = true },
+                new Authority { Id = 5, Name = "Brand List Authority", Active = true },
+                new Authority { Id = 6, Name = "Model List Authority", Active = true }
+            );
+
+            modelBuilder.Entity<Role>().HasData(
+              new Role { Id = 1, Name = "System Administrator", Active = true },
+              new Role { Id = 2, Name = "Vehicle Administrator", Active = true },
+              new Role { Id = 3, Name = "Brand Administrator", Active = true },
+              new Role { Id = 4, Name = "Model Administrator", Active = true },
+              new Role { Id = 5, Name = "User", Active = true }
+          );
+
+            modelBuilder.Entity<AuthRole>().HasData(
+                new AuthRole { Id = 1, AuthorityId = 1, RoleId = 1, Active = true },
+                new AuthRole { Id = 2, AuthorityId = 2, RoleId = 1, Active = true },
+                new AuthRole { Id = 3, AuthorityId = 3, RoleId = 1, Active = true },
+                new AuthRole { Id = 4, AuthorityId = 4, RoleId = 1, Active = true },
+                new AuthRole { Id = 5, AuthorityId = 5, RoleId = 1, Active = true },
+                new AuthRole { Id = 6, AuthorityId = 6, RoleId = 1, Active = true },
+
+                new AuthRole { Id = 7, AuthorityId = 1, RoleId = 2, Active = true },
+                new AuthRole { Id = 8, AuthorityId = 2, RoleId = 2, Active = true },
+                new AuthRole { Id = 9, AuthorityId = 3, RoleId = 2, Active = true },
+                new AuthRole { Id = 10, AuthorityId = 4, RoleId = 2, Active = true },
+
+                new AuthRole { Id = 11, AuthorityId = 5, RoleId = 3, Active = true },
+
+                new AuthRole { Id = 12, AuthorityId = 6, RoleId = 4, Active = true },
+
+                new AuthRole { Id = 13, AuthorityId = 4, RoleId = 5, Active = true },
+                new AuthRole { Id = 14, AuthorityId = 5, RoleId = 5, Active = true },
+                new AuthRole { Id = 15, AuthorityId = 6, RoleId = 5, Active = true }
+            );
+
+            modelBuilder.Entity<User>().HasData(
+              new User { Id = 1, Name = "Okan Kaan Cetinkaya", Password = "1234", EMail = "okankaan.cetinkaya@gmail.com", Active = true },
+              new User { Id = 2, Name = "Maria Anders", Password = "1234", EMail = "maria.anders@test.com", Active = true },
+              new User { Id = 3, Name = "Antonio Moreno Taquería", Password = "1234", EMail = "antonio.moreno@test.com", Active = true },
+              new User { Id = 4, Name = "Sven Ottlieb", Password = "1234", EMail = "sven.ottlieb@test.com", Active = true },
+              new User { Id = 5, Name = "Carine Schmitt", Password = "1234", EMail = "carine.schmitt@test.com", Active = true }
+          );
+
+            modelBuilder.Entity<UserRole>().HasData(
+              new UserRole { Id = 1, UserId = 1, RoleId = 1, Active = true },
+              new UserRole { Id = 2, UserId = 2, RoleId = 2, Active = true },
+              new UserRole { Id = 3, UserId = 3, RoleId = 3, Active = true },
+              new UserRole { Id = 4, UserId = 4, RoleId = 4, Active = true },
+              new UserRole { Id = 5, UserId = 5, RoleId = 5, Active = true }
+          );
+
+            modelBuilder.Entity<Color>().HasData(
+              new Color { Id = 1, Name = "White", Active = true },
+              new Color { Id = 2, Name = "Red", Active = true },
+              new Color { Id = 3, Name = "Night Blue", Active = true },
+              new Color { Id = 4, Name = "Black", Active = true },
+              new Color { Id = 5, Name = "Space Grey", Active = true }
+          );
+
+            modelBuilder.Entity<Brand>().HasData(
+              new Brand { Id = 1, Name = "Bmw", Active = true },
+              new Brand { Id = 2, Name = "Toyota", Active = true },
+              new Brand { Id = 3, Name = "Fiat", Active = true },
+              new Brand { Id = 4, Name = "Opel", Active = true },
+              new Brand { Id = 5, Name = "Audi", Active = true }
+          );
+
+            modelBuilder.Entity<Model>().HasData(
+              new Model { Id = 1, Name = "Yaris", BrandId = 2, Active = true },
+              new Model { Id = 2, Name = "Rav 4", BrandId = 2, Active = true },
+              new Model { Id = 3, Name = "x5", BrandId = 1, Active = true },
+              new Model { Id = 4, Name = "Corsa", BrandId = 4, Active = true },
+              new Model { Id = 5, Name = "Corolla", BrandId = 2, Active = true }
+          );
+
+            modelBuilder.Entity<Vehicle>().HasData(
+             new Vehicle { Id = 1, UserId = 1, ModelId = 2, ColorId = 2, Price = 200000, Active = true },
+             new Vehicle { Id = 2, UserId = 1, ModelId = 1, ColorId = 5, Price = 150000, Active = true },
+             new Vehicle { Id = 3, UserId = 5, ModelId = 3, ColorId = 4, Price = 230000, Active = true },
+             new Vehicle { Id = 4, UserId = 4, ModelId = 4, ColorId = 2, Price = 500000, Active = true },
+             new Vehicle { Id = 5, UserId = 3, ModelId = 2, ColorId = 2, Price = 200000, Active = true },
+             new Vehicle { Id = 6, UserId = 3, ModelId = 5, ColorId = 3, Price = 300000, Active = true }
+         );
+        }
+
     }
 }
